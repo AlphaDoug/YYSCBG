6e4d53678eaa6d1aa6be071b155f8ceb9ee41e6a
AlphaDoug <519558938@qq.com>
2020-09-21 02:32:15 +0800

    角色数据

--- CrawlYYSCBG_RoleDataList/main.py
+++ CrawlYYSCBG_RoleDataList/main.py
@@ -1,16 +1,16 @@
 import json
-import os
 import requests
 
 
-def getCbgData():
+def getCbgData(server_id, order_sn):
+    # 根据服务器ID和角色唯一ID获取指定的角色的藏宝阁数据
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
         'Refer': 'https://yys.cbg.163.com/cgi/mweb/equip/11/202008232001616-11-VTGMKTV8K4VDS?view_loc=all_list%7Ctag_key%3A%7B%22sort_key%22%3A%20%22recommd%22,%20%22tag%22%3A%20%22auto-gen%22,%20%22sort_order%22%3A%20%22%22%7D&tag=auto-gen'
     }
     params = {
-        'serverid': 36,
-        'ordersn': '202006260801616-36-GSQPY167J8IAD',
+        'serverid': server_id,
+        'ordersn': order_sn,
         'view_loc': 'reco_home|tag_key:{"sort_key": "recommd", "tag": "auto-gen", "sort_order": ""}'
     }
     r = requests.get(url='https://yys.cbg.163.com/cgi/api/get_equip_detail',
@@ -18,37 +18,24 @@ def getCbgData():
     cont = r.json()
     equip_desc = cont['equip']['equip_desc']
     equip_desc = equip_desc.encode().decode('unicode_escape')
-    equip_desc_json = json.loads(equip_desc)
-    return cont, equip_desc_json
+    try:
+        equip_desc_json = json.loads(equip_desc)
+    except json.decoder.JSONDecodeError:
+        print('json转化失败')
+        return {}
+    cont['equip']['equip_desc'] = None
+    info = {}
+    info['other_info'] = cont
+    info['equip_desc'] = equip_desc_json
+    return info
 
 
-def saveData(_contJson, _equip_descJson):
+def saveData(info):
     # 将藏宝阁数据保存
     print('将藏宝阁数据保存')
-    with open('../cont.json', 'w', encoding='utf-8') as file:
-        file.write(json.dumps(_contJson, indent=2, ensure_ascii=False))
+    if not info:
+        return
+    key = info['other_info']['equip']['game_ordersn']
+    with open('Data/RoleDataList/' + key + '.json', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(info, indent=2, ensure_ascii=False))
         file.close()
-    with open('../equip_desc.json', 'w', encoding='utf-8') as file:
-        file.write(json.dumps(_equip_descJson, indent=2, ensure_ascii=False))
-        file.close()
-
-
-def getFileData():
-    file1 = open('../cont.json', encoding='utf-8')
-    cont = file1.read()
-    file2 = open('../equip_desc.json', encoding='utf-8')
-    equip_desc = file2.read()
-    file1.close()
-    file2.close()
-    js1 = json.loads(cont)
-    js2 = json.loads(equip_desc)
-    return js1, js2
-
-
-def readData():
-    if not os.path.isfile('../cont.json') or not os.path.isfile('../equip_desc.json'):
-        os.path.join('../cont.json')
-        os.path.join('../equip_desc.json')
-        cont, equip = getCbgData()
-        saveData(cont, equip)
-    return getFileData()
--- main.py
+++ main.py
@@ -1,50 +1,73 @@
 import re
 import CrawlYYSCBG_Roles.main
+import CrawlYYSCBG_RoleDataList.main
 import xlwt
+import time
+import random
 
-allRoles = CrawlYYSCBG_Roles.main.GetAllUserData()
-allRolesInfo = []
-for role in allRoles:
-    oneRoleInfo = {}
-    oneRoleInfo['level'] = role['equip_level']
-    oneRoleInfo['area_name'] = role['area_name']
-    oneRoleInfo['server_name'] = role['server_name']
-    oneRoleInfo['price'] = role['price'] * 0.01
-    oneRoleInfo['ssr_num'] = int(role['other_info']['basic_attrs'][0].split(' ')[1])
-    oneRoleInfo['six_star_num'] = int(role['other_info']['basic_attrs'][1].split(' ')[1])
-    oneRoleInfo['sign_in_day'] = re.sub('\D', '', role['other_info']['basic_attrs'][2])
-    oneRoleInfo['collect_num'] = role['collect_num']
-    oneRoleInfo['format_equip_name'] = role['format_equip_name']
-    if role['platform_type'] == 1:
-        oneRoleInfo['platform_type'] = 'IOS'
-    else:
-        oneRoleInfo['platform_type'] = '安卓'
-    allRolesInfo.append(oneRoleInfo)
-wb = xlwt.Workbook()
-ws = wb.add_sheet('所有上架角色数据')
-row = 0
-ws.write(row, 0, '名字')
-ws.write(row, 1, '等级')
-ws.write(row, 2, '区域名称')
-ws.write(row, 3, '服务器')
-ws.write(row, 4, '平台')
-ws.write(row, 5, '价格')
-ws.write(row, 6, 'SSR数量')
-ws.write(row, 7, '六星数量')
-ws.write(row, 8, '签到天数')
-ws.write(row, 9, '收藏数量')
-row = row + 1
 
-for one_row in allRolesInfo:
-    ws.write(row, 0, one_row['format_equip_name'])
-    ws.write(row, 1, one_row['level'])
-    ws.write(row, 2, one_row['area_name'])
-    ws.write(row, 3, one_row['server_name'])
-    ws.write(row, 4, one_row['platform_type'])
-    ws.write(row, 5, one_row['price'])
-    ws.write(row, 6, one_row['ssr_num'])
-    ws.write(row, 7, one_row['six_star_num'])
-    ws.write(row, 8, one_row['sign_in_day'])
-    ws.write(row, 9, one_row['collect_num'])
+def SaveDataAsExcel():
+    # 将读取藏宝阁中的数据，进行一定会处理后保存在表格中
+    allRoles = CrawlYYSCBG_Roles.main.GetAllUserData()
+    allRolesInfo = []
+    for role in allRoles:
+        oneRoleInfo = {}
+        oneRoleInfo['level'] = role['equip_level']
+        oneRoleInfo['area_name'] = role['area_name']
+        oneRoleInfo['server_name'] = role['server_name']
+        oneRoleInfo['price'] = role['price'] * 0.01
+        oneRoleInfo['ssr_num'] = int(role['other_info']['basic_attrs'][0].split(' ')[1])
+        oneRoleInfo['six_star_num'] = int(role['other_info']['basic_attrs'][1].split(' ')[1])
+        oneRoleInfo['sign_in_day'] = re.sub('\D', '', role['other_info']['basic_attrs'][2])
+        oneRoleInfo['collect_num'] = role['collect_num']
+        oneRoleInfo['format_equip_name'] = role['format_equip_name']
+        if role['platform_type'] == 1:
+            oneRoleInfo['platform_type'] = 'IOS'
+        else:
+            oneRoleInfo['platform_type'] = '安卓'
+        allRolesInfo.append(oneRoleInfo)
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet('所有上架角色数据')
+    row = 0
+    ws.write(row, 0, '名字')
+    ws.write(row, 1, '等级')
+    ws.write(row, 2, '区域名称')
+    ws.write(row, 3, '服务器')
+    ws.write(row, 4, '平台')
+    ws.write(row, 5, '价格')
+    ws.write(row, 6, 'SSR数量')
+    ws.write(row, 7, '六星数量')
+    ws.write(row, 8, '签到天数')
+    ws.write(row, 9, '收藏数量')
     row = row + 1
-wb.save('Data/AllRoles.xls')
+
+    for one_row in allRolesInfo:
+        ws.write(row, 0, one_row['format_equip_name'])
+        ws.write(row, 1, one_row['level'])
+        ws.write(row, 2, one_row['area_name'])
+        ws.write(row, 3, one_row['server_name'])
+        ws.write(row, 4, one_row['platform_type'])
+        ws.write(row, 5, one_row['price'])
+        ws.write(row, 6, one_row['ssr_num'])
+        ws.write(row, 7, one_row['six_star_num'])
+        ws.write(row, 8, one_row['sign_in_day'])
+        ws.write(row, 9, one_row['collect_num'])
+        row = row + 1
+    wb.save('Data/AllRoles.xls')
+
+
+def SaveAllRolesData():
+    # 根据订单号获取所有的角色的详细数据并保存
+    allRoles = CrawlYYSCBG_Roles.main.GetAllUserData()
+    all_order_sn = []
+    for role in allRoles:
+        one_data = {'order_sn': role['game_ordersn'], 'server_id': role['serverid']}
+        all_order_sn.append(one_data)
+
+    for data in all_order_sn:
+        time.sleep(random.random() * 2 + 2)
+        role_data = CrawlYYSCBG_RoleDataList.main.getCbgData(data['server_id'], data['order_sn'])
+        CrawlYYSCBG_RoleDataList.main.saveData(role_data)
+
+
+SaveAllRolesData()
