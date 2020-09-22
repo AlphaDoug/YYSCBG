72cc00dfcd129842945ac9ab005b548b89001a13
lilith-avatar-sharifma <sharifma@lilith.com>
2020-09-22 15:09:03 +0800

    数据

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import re
-
 import requests
-
 import CrawlYYSCBG_Roles.main
 import CrawlYYSCBG_RoleDataList.main
-import xlwt
 import time
-import random
+import xlwt
 
 
 def SaveDataAsExcel():
@@ -74,37 +70,3 @@ def SaveAllRolesData():
 
 
 SaveAllRolesData()
-
-proxypool_url = 'http://127.0.0.1:5555/random'
-target_url = 'http://httpbin.org/get'
-
-def get_random_proxy():
-    """
-    get random proxy from proxypool
-    :return: proxy
-    """
-    return requests.get(proxypool_url).text.strip()
-
-def crawl(url, proxy):
-    """
-    use proxy to crawl page
-    :param url: page url
-    :param proxy: proxy, such as 8.8.8.8:8888
-    :return: html
-    """
-    proxies = {'http': 'http://' + proxy}
-    return requests.get(url, proxies=proxies).text
-
-
-def main():
-    """
-    main method, entry point
-    :return: none
-    """
-    proxy = get_random_proxy()
-    print('get random proxy', proxy)
-    html = crawl(target_url, proxy)
-    print(html)
-
-if __name__ == '__main__':
-    main()
